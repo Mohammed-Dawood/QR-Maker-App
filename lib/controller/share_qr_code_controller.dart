bd91fd04f4af8c7daf3a503835de1ea64e405bb5
Mohammed Dawood <mohammeddawood@MacBook-Pro-som-tillhor-Mohammed.local>
2022-08-19 04:25:38 +0200

    edit share pluse

--- lib/controller/share_qr_code_controller.dart
+++ lib/controller/share_qr_code_controller.dart
@@ -5,14 +5,19 @@ import 'package:share_plus/share_plus.dart';
 import 'package:path_provider/path_provider.dart';
 
 class ShareQrCodeController extends GetxController {
-  Future shareQrCode(Uint8List bytes) async {
-    final directory = await getApplicationDocumentsDirectory();
-    final image = File('${directory.path}/flutter.png');
-    image.writeAsBytesSync(bytes);
+  Future shareQrCode(Uint8List image) async {
+    // final directory = await getApplicationDocumentsDirectory();
+    // final image = File('${directory.path}/flutter.png');
+    // image.writeAsBytesSync(bytes);
+
+    final directory = await getTemporaryDirectory();
+    final path = '${directory.path}/image.jpg';
+    File(path).writeAsBytesSync(image);
+
     await Share.shareFiles(
-      [image.path],
-      text: 'QR Code',
-      subject: 'QR Code',
+      [path],
+      text: 'QR Maker App',
+      subject: 'QR Maker App',
     );
   }
 }
